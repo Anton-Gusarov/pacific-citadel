656c598eca249bfaa9430b9ab02ee468eb855413
anton <anton.v.gusarov@gmail.com>
2014-06-05 10:24:31 +0400

    app init

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,76 @@
 /* Author: YOUR NAME HERE
 */
 
-$(document).ready(function() {
+app = new Backbone.Marionette.Application();
+
+app.module("Layout", function (Layout, App, Backbone) {
+
+    Layout.ControlView = Backbone.Marionette.View.extend({
+
+        template: "#controlTemplate",
+
+        ui: {
+            formUrl: ".Url-Form",
+            inputUrl: ".Url-Input",
+            buttonUrl: ".Url-Button"
+        },
+
+        events: {
+            "click @ui.buttonUrl": "submitUrl"
+        },
+
+        submitUrl: function (e) {
+            e.preventDefault();
+            this.model.set("url", this.ui.inputUrl.val());
+            app.vent.trigger("change:url", {
+                value: this.model.get("url")
+            });
+        }
+
+    });
+
+    Layout.IframeView = Backbone.Marionette.View.extend({
+
+        ui: {
+            iframe: "#content"
+        },
+
+        modelEvents: {
+            "change:url": "setUrl"
+        },
+
+        setUrl: function () {
+            this.ui.iframe.attr('src', this.model.get("url"));
+        }
+
+    });
+
+});
+
+app.module("Controller", function (Controller, App, Backbone) {
+
+    var model = new Backbone.Model();
+
+    Controller.addInitializer(function () {
+
+        var control = new app.Layout.ControlView({
+            model: model
+        });
+        app.controlRegion.show(control);
+
+        var iframe = new app.Layout.IframeView({
+            model: model
+        });
+        app.iframeRegion.show(iframe);
+    });
+
+});
+
+app.addRegions({
+    "controlRegion": "#control",
+    "iframeRegion": ".Iframe"
+});
+
 
     /*var socket = io.connect();
 
@@ -12,4 +81,5 @@ $(document).ready(function() {
     socket.on('server_message', function(data){
      $('#receiver').append('<li>' + data + '</li>');
     });*/
-});
+
+//    app.start();
